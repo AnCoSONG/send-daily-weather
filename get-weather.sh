45da89768f316c0f5b26e3440363f7dbc931d6d8
AnCoSONG <1197252996@qq.com>
2021-02-18 19:31:43 +0800

    feat: 🎸 add get-weather

---
+++ get-weather.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+CITY=成都
+LANGUAGE="zh-CN"
+UNIT=M
+UA="Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36"
+if test -z "$1"
+then
+    echo "使用默认CITY:$CITY"
+else
+    CITY=$1
+    echo $CITY
+fi
+
+curl \
+  -H "Accept-Language: $LANGUAGE" \
+  -H "User-Agent: $UA" \
+  -o "result-$CITY.html" \
+  wttr.in/$CITY?format=4\&$UNIT
+
+# 想要去掉format可能需要用js编辑返回的html文件，源文件引入了一个非常沙雕的外部css完全可以手动写入
