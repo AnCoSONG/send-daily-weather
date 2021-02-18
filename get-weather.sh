dc1d6c13a2f3fb619648246de051bba8c176dd62
AnCoSONG <1197252996@qq.com>
2021-02-18 20:15:18 +0800

    fix: 🐛 using celsius

--- get-weather.sh
+++ get-weather.sh
@@ -2,7 +2,6 @@
 
 CITY=成都
 LANGUAGE="zh-CN"
-UNIT=M
 UA="Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36"
 if test -z "$1"
 then
@@ -16,6 +15,6 @@ curl \
   -H "Accept-Language: $LANGUAGE" \
   -H "User-Agent: $UA" \
   -o "result-$CITY.html" \
-  wttr.in/$CITY?format=4\&$UNIT
+  wttr.in/$CITY?format=4\&m
 
 # 想要去掉format可能需要用js编辑返回的html文件，源文件引入了一个非常沙雕的外部css完全可以手动写入
